0b188e19e7170c840f1db3bc7e477f4dc0112fca
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-09-20 17:41:25 +0200

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void MyFifthMethod(string firstName, string lastName)
+    {
+        Console.WriteLine(lastName + ", " + firstName);
+    }
+
+
+    // No Main
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your firstName");
+        string userfirstName = Console.ReadLine();
+        Console.WriteLine("Please enter your lastName");
+        string userlastName = Console.ReadLine();
+        MyFifthMethod(userfirstName, userlastName) ;
+
+    }
+}
